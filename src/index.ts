a91e61db1cc466a2076ad0c5edb73808b02ef015
rajat-blr <vrajatlink@gmail.com>
2025-08-06 19:14:05 +0530

    Initial setup

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+
+app.post("/api/v1/signin", (req, res)=>{
+
+})
+
+app.post("api/v1/content", (req,res)=>{
+
+})
+
+app.post("/api/v1/brain/share",(req,res)=>{
+    
+})
