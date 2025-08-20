2ec93677beb78cbe3cf6505f6d5bbf1764ef3d03
rajat-blr <vrajatlink@gmail.com>
2025-08-20 16:32:00 +0530

    a little cluttered frontend setup

---
+++ brainly-fe/src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+        <div className="text-3xl font-bold underline">
+              HIiiiii
+        </div>
+    </>
+  )
+}
+
+export default App
